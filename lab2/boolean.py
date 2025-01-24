d0a94b19e3b231b8af8b82df42f7dd425ca163ef
6UCEH <6uceh7@gmail.com>
2025-01-24 12:46:55 +0500

    lab2

---
+++ lab2/boolean.py
@@ -0,0 +1,26 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+print("\n")
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print("\n")
+x = 200.11
+print(isinstance(x, int))
+
+print("\n")
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
