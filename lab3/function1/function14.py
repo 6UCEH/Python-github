5f840068a68e850f96cf384b45a10490575f4f9c
6UCEH <6uceh7@gmail.com>
2025-01-31 16:51:01 +0500

    asd

--- lab3/function1/function14.py
+++ lab3/function1/function14.py
@@ -1,10 +1,17 @@
-
 from function09 import sphere_volume
+from function06 import reverse_words
+from function01 import grams_to_ounces
 
+#  reverse_words
+sentence = input("Введите предложение: ")
+result = reverse_words(sentence)
+print("Результат:", result)
 
+#  grams_to_ounces
+grams = 100
+ounces = grams_to_ounces(grams)
+print(grams, "граммов =", round(ounces, 2), "унции")
 
-
+#  функции sphere_volume
 radius = 3
-print("Volume of the sphere with radius", radius, ":", round(sphere_volume(radius), 2))
-
-
+print("Объем сферы с радиусом", radius, "=", round(sphere_volume(radius), 2))
