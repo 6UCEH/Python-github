39d7e7f2654b15338b9d9dd1ca5b93d281a99217
6UCEH <6uceh7@gmail.com>
2025-01-31 17:05:18 +0500

    c

--- lab3/function1/function01.py
+++ lab3/function1/function01.py
@@ -2,6 +2,6 @@ def grams_to_ounces(grams):
     return grams / 28.3495231
 
 
-grams = 100
+grams = float(input("\nHow many gram?\n"))
 ounces = grams_to_ounces(grams)
 print(grams, "граммов =", round(ounces, 2), "унции")
--- lab3/function1/function14.py
+++ lab3/function1/function14.py
@@ -3,15 +3,14 @@ from function06 import reverse_words
 from function01 import grams_to_ounces
 
 #  reverse_words
-sentence = input("Введите предложение: ")
+sentence = ""
 result = reverse_words(sentence)
-print("Результат:", result)
+
 
 #  grams_to_ounces
-grams = 100
+grams = 10
 ounces = grams_to_ounces(grams)
-print(grams, "граммов =", round(ounces, 2), "унции")
+
 
 #  функции sphere_volume
 radius = 3
-print("Объем сферы с радиусом", radius, "=", round(sphere_volume(radius), 2))
